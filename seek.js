333195a566a9274c4ca9faae484092e88232b2ee
jackhowa <johncarverhoward@gmail.com>
2017-08-10 16:21:42 -0500

    use filter to try to find the matches with includes

--- seek.js
+++ seek.js
@@ -1,7 +1,9 @@
 
-function destroyer(arr) {
-  // Remove all the values
-  return arr;
+function destroyer(potentiallyDestroyedArray) {
+  return potentiallyDestroyedArray[0].filter(function(potentialDestroy) {
+    return potentiallyDestroyedArray[1].includes(potentialDestroy);
+  });
+
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
