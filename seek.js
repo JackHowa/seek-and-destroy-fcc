b893f910a6e0a2af679f2246a115246209c5949e
jackhowa <johncarverhoward@gmail.com>
2017-08-10 16:40:04 -0500

    found problems with the setting up inital array into variables

--- seek.js
+++ seek.js
@@ -1,9 +1,18 @@
 function destroyer(destroyableInputs) {
   var potentialDeletes = [destroyableInputs[0]];
-  var valuesToDelete = [destroyableInputs[1], destroyableInputs[1]];
+  // ah this is only 1 
+  // not getting the whole array 
 
-  return potentialDeletes.filter(function(potentialDestroy) {
-    return valuesToDelete.indexOf(potentialDestroy) > -1;
+  var valuesToDelete = [destroyableInputs[1], destroyableInputs[2]];
+  // am getting the first two here 
+
+
+  console.log(potentialDeletes);
+  console.log(valuesToDelete);
+
+  var undeletedElements = potentialDeletes.filter(function(potentialDestroy) {
+    return (valuesToDelete.indexOf(potentialDestroy) > -1);
   });
+  return undeletedElements;
 }
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
