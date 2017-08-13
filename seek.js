fb342bd184a62084f1da01a851190746714cb2e3
jackhowa <johncarverhoward@gmail.com>
2017-08-13 14:33:04 -0500

    use a wider range to find matches and not to find those that aren't included

--- seek.js
+++ seek.js
@@ -2,12 +2,16 @@ function destroyer(destroyableInputs) {
   var args = Array.prototype.slice.call(arguments);
 
   var potentialDeletes = args[0];
-  var valuesToDelete = [args[1],args[2]];
+  // need to parse everything after index zero not just 1 and 2 
 
 
-  // var undeletedElements = potentialDeletes.filter(function(potentialDestroy) {
-  //   return (valuesToDelete.indexOf(potentialDestroy) > -1);
-  // });
-  // return undeletedElements;
+  var valuesToDelete = [args[1],args[2], args[3]];
+
+  var undeletedElements = potentialDeletes.filter(function(potentialDestroy) {
+    // use not to delete falses 
+    // index of finds not in the other array as -1
+    return !(valuesToDelete.indexOf(potentialDestroy) > -1);
+  });
+  return undeletedElements;
 }
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
