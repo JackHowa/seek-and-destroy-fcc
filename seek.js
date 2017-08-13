e974c2e3302998a795c432cc3d9de2ee4877ed86
jackhowa <johncarverhoward@gmail.com>
2017-08-13 14:26:56 -0500

    restarting with slice object arguments array

--- seek.js
+++ seek.js
@@ -1,18 +1,11 @@
 function destroyer(destroyableInputs) {
-  var potentialDeletes = [destroyableInputs[0]];
-  // ah this is only 1 
-  // not getting the whole array 
+  var arr = Array.prototype.slice.call(arguments);
+  var args = Array.from(arguments);
 
-  var valuesToDelete = [destroyableInputs[1], destroyableInputs[2]];
-  // am getting the first two here 
-
-
-  console.log(potentialDeletes);
-  console.log(valuesToDelete);
-
-  var undeletedElements = potentialDeletes.filter(function(potentialDestroy) {
-    return (valuesToDelete.indexOf(potentialDestroy) > -1);
-  });
-  return undeletedElements;
+  console.log(args);
+  // var undeletedElements = potentialDeletes.filter(function(potentialDestroy) {
+  //   return (valuesToDelete.indexOf(potentialDestroy) > -1);
+  // });
+  // return undeletedElements;
 }
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
